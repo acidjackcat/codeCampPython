c0cc87b6a78c8533167cd355e14308ff021dad5d
AcidJackCat <isadov.test03@gmail.com>
2019-02-06 23:38:21 +0200

    First commit

---
+++ venv/coins.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+found_coins = 20
+magic_coins = 70
+stolen_coins = 3
+coins = found_coins
+for week in range(1, 53):
+    coins = coins + magic_coins - stolen_coins
+    print('Неделя %s = %s' % (week, coins))
+
---
+++ venv/pants.py
@@ -0,0 +1,7 @@
+hugehairypants = ['огромные', 'волосатые', 'штаны']
+for i in hugehairypants:
+    print(i)
+    for j in hugehairypants:
+        print(j)
+
+
