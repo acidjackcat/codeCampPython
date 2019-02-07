afe30eb6e8e72a5043ca20772afd914b041dc362
AcidJackCat <isadov.test03@gmail.com>
2019-02-07 13:51:45 +0200

    Remove ignored files.

--- venv/Cars_p1.py
+++
@@ -1,7 +0,0 @@
-fname = 'Igor'
-lname = 'Awesome'
-print(('Hello {0}, your full name is {0} {1}'.format(fname, lname))*5)
-
-
-geeks = 'Geeks for Geeks'
-print(('{0}'.format(geeks))*3)
--- venv/pants.py
+++
@@ -1,7 +0,0 @@
-hugehairypants = ['огромные', 'волосатые', 'штаны']
-for i in hugehairypants:
-    print(i)
-    for j in hugehairypants:
-        print(j)
-
-
